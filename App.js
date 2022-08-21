c5725a360e8d3c48d88fe6d2054159d39421b417
atif <atif@emai.com>
2022-08-21 23:53:24 +0530

    git init

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  FlatList,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
-export default function App() {
+function App() {
+  const [list, setList] = useState(["milk", "coffee", "oranges"]);
+  const [finalList, setFinalList] = useState(["milk", "coffee", "oranges"]);
+  const [textInput, setTextInput] = useState("");
+  const onChangeText = (text) => {
+    setTextInput(text);
+    const regex = new RegExp("^" + text, "i");
+    const matchedSites = finalList.filter((e) => regex.test(e));
+    setList(matchedSites);
+  };
+  const handleAddItem = () => {
+    if (textInput) {
+      setList((prevState) => [textInput, ...finalList]);
+      setFinalList((prevState) => [textInput, ...prevState]);
+      setTextInput("");
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 50 }}>
+      <View style={{ flexDirection: "row" }}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={textInput}
+        />
+        <TouchableOpacity
+          style={{ padding: 5, borderWidth: 2, alignSelf: "center" }}
+          onPress={handleAddItem}
+        >
+          <Text style={{ fontSize: 30 }}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList data={list} renderItem={ListItem} />
     </View>
   );
 }
+const ListItem = ({ item, index }) => {
+  return (
+    <View style={{ padding: 30, borderBottomWidth: 2 }}>
+      <Text>{item}</Text>
+    </View>
+  );
+};
+``;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    width: "80%",
+    height: 43,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
+export default App;
